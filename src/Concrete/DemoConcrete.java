5ed33cb8818cee66159ef2b86761e9ee206334ed
Owner <Owner@10.30.17.199>
2013-08-28 18:57:35 -0500

    Demo the code of the Concrete Package

---
+++ src/Concrete/DemoConcrete.java
@@ -0,0 +1,17 @@
+/*
+ * Demo working Concrete code.
+ */
+package Concrete;
+
+/**
+ *
+ * Teresa Mahoney
+ */
+public class DemoConcrete {
+    public static void main(String[] args) {
+        SalariedEmployee sal1= new SalariedEmployee("Herb Hanover","Accounting",
+                "Sr Mgr",50000);
+        System.out.println(sal1.toString());
+    }
+  
+}
