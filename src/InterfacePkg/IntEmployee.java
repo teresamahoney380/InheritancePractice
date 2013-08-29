d17789e548638b0bdde2465dfb83c832e1b6f42d
Owner <Owner@10.30.17.199>
2013-08-29 14:30:50 -0500

    Add Interface Class

---
+++ src/InterfacePkg/IntEmployee.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfacePkg;
+
+/**
+ *
+ * @author Owner
+ */
+public interface IntEmployee {
+    // Methods
+    public String geteName(); 
+
+    public void seteName(String eName); 
+
+    public String geteDepartment(); 
+
+    public void seteDepartment(String eDepartment); 
+
+    public String getePosition(); 
+
+    public void setePosition(String ePosition); 
+    
+
+    public double getWeeklyPay(); 
+
+    public void setWeeklyPay(double weeklyPay); 
+
+    public String toString(); 
+    
+}
