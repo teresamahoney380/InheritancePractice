a88a69202f09b2bad382d64bd03fd0c3231dda4b
Owner <Owner@10.30.17.199>
2013-08-28 19:08:02 -0500

    Trying to insert printlns to see where code is failing

--- src/Concrete/DemoConcrete.java
+++ src/Concrete/DemoConcrete.java
@@ -9,8 +9,10 @@ package Concrete;
  */
 public class DemoConcrete {
     public static void main(String[] args) {
+        System.out.println("first line of demo");
         SalariedEmployee sal1= new SalariedEmployee("Herb Hanover","Accounting",
                 "Sr Mgr",50000);
+        System.out.println("YOU got this much done");
         System.out.println(sal1.toString());
     }
   
